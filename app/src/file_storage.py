5b6a449358608f7bb1e989f55d5365a950b4936f
Cao Quynh An <ancaoquynh@gmail.com>
2024-04-14 19:31:17 +0200

    Update code read+add events

---
+++ app/src/file_storage.py
@@ -0,0 +1,28 @@
+import json
+
+
+
+class EventFileManager:
+    FILE_PATH = "event.json"
+
+    @classmethod
+    def read_events_from_file(cls):
+        try:
+            with open(cls.FILE_PATH, "r") as file:
+                events_data = json.load(file)
+                return events_data
+        except FileNotFoundError:
+            return []
+        except Exception as e:
+            print(f"Error reading events file: {e}")
+            return []
+        
+    @classmethod
+    def write_events_to_file(cls, events):
+        try:
+            with open(cls.FILE_PATH, "w") as file:
+                json.dump(events, file, indent=4)
+        except Exception as e:
+            print(f"Error writing events file: {e}")
+        
+    
